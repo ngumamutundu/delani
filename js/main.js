a9b15c80dd9f8f3278ef09a5f89b04ca15dd2219
pmutundu <mutundu.nguma@gmail.com>
2022-11-24 08:39:57 +0300

    new delani

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$("document").ready(function(){
+    $(".portfolio-img").hover(function(e){
+      const target =e.target;
+
+      if(target.matches("img")){
+
+        const titlename = target.alt.toUpperCase();
+        $(target).pop("title", titlename)
+      }
+    })
+})
+   
+    
